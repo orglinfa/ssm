87dce77de734d4212aa4ff15abdd526a0893a0e9
orglinfa <lingfeian@aliyun.com>
2018-04-21 20:48:22 +0800

    first commit;

--- src/main/java/org/ssm/web/IndexController.java
+++ src/main/java/org/ssm/web/IndexController.java
@@ -1,8 +1,11 @@
 package org.ssm.web;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.ssm.dao.UserInfoMapper;
 import org.ssm.pojo.UserInfo;
@@ -10,6 +13,7 @@ import org.ssm.pojo.UserInfo;
 @Controller
 @RequestMapping("/protal")
 public class IndexController {
+    private  static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
     @Autowired
     public UserInfoMapper UserInfoMapper;
     @RequestMapping("/index")
@@ -17,7 +21,7 @@ public class IndexController {
     public String index(){
         System.out.println("========================");
         UserInfo userInfo=new UserInfo();
-        userInfo.setId("1");
+        userInfo.setId("2");
         userInfo.setUsername("test");
         System.out.println(UserInfoMapper);
         try{
@@ -28,5 +32,13 @@ public class IndexController {
 
         System.out.println("=====================");
         return "index str";
+    }
+    @RequestMapping(value = "/getUser")
+    @ResponseBody
+    public UserInfo getUserInfo(@RequestParam  String id){
+        System.out.println("==============="+id);
+        System.out.println(logger);
+        logger.error("===============查询用户id：==============="+id);
+        return UserInfoMapper.selectByPrimaryKey(id);
     }
 }
